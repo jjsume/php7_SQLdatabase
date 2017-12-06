eef20342e531bb92ffc1862c29fbfd245c86340a
jjsume <34176980+jjsume@users.noreply.github.com>
2017-12-06 19:48:50 +0200

    Updated 06_12_2017
    Added __destruct-method and code comments to Database.php.
    Updated database test file also.

--- Database.php
+++ Database.php
@@ -1,5 +1,9 @@
 <?php
-//Class for Database using.
+/* Class for Database using.
+ *
+ * When new Database instace will be created, there's a createConnection-method
+ * called from the constructor.
+ */
 
 class Database {
 
@@ -10,18 +14,22 @@ class Database {
 	var $password;
 	var $database;
 
-	//Constructor
+	/* Constructor
+	 * 
+ 	 * There's four input parameters given when 'new' keyword is used.
+    	 */
 	function __construct($server,$user,$password, $database) {
 		$this->server = $server;
 		$this->user = $user;
 		$this->password =  $password;
 		$this->database = $database;
+		//Let's call createConnection function defined later in this class.
 		$this::createConnection($server,$user,$password,$database);	
 	 }
 
 	
 	//Other methods
-	//Next function creates a connection
+	//Next function creates a connection (PDO solution)
 	function createConnection($server,$user,$password,$database) {	
 
 
@@ -39,6 +47,16 @@ class Database {
 	
 	}
 
+	/*__destruct method is activated when garbage collector is activated.
+	 * (Object is died itself, killed by someone, or there's no more references pointing 
+	 * to the object at the memory.)
+	 */
+
+	function __destruct() {
+	//In the PDO solution closing database happens by setting active PDO-connection-object as 'null'.
+		$this->connection = null;	 
+		echo "Connection to database is closed!";
+	}
 }
 
 ?>
--- Test_DatabaseClass.php
+++ Test_DatabaseClass.php
@@ -3,7 +3,17 @@ include('Database.php');
 
 $server ='localhost';	//Database server name goes here! localhost:XXXX
 $user='root';		//Database user name goes here!
-$password='vieras';	//Database user's password
-$database = 'oma';	//Database name goes here
+$password='password';	//Database user's password
+$database = 'userDB';	//Database name goes here
 $con = new Database($server,$user,$password,$database); //Now we create new Database-object.
+$conB = $con->connection;	//Now $conB refers to the PDO-object stored at the memory
+
+/* 
+ * Next few examples are for MySQL-use
+ *
+ * $query("INSERT INTO User(fName,lName,SSN) VALUES('John','Doe','061217-6667');";
+ * $conB->exec($query);	//Actually this one calls PDO-Connection Object method '. 
+ */
+		
+
 ?>
